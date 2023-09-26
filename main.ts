20262803298095d3231516c01b43f0f694abfac9
dharding5 <146109844+dharding5@users.noreply.github.com>
2023-09-26 10:08:28 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let bruce_21_avatar = sprites.create(img`
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+    3 3 f f f 3 3 3 3 3 3 3 f f f 3 
+    3 3 f 7 f 3 3 f f f 3 3 f 7 f 3 
+    3 3 f 7 f f f f 3 f f f f 7 f 3 
+    3 3 f f f f f f f f f f f f f 3 
+    3 3 f 1 1 1 1 1 1 1 1 1 1 1 f 3 
+    3 3 f 1 1 9 9 1 1 1 9 9 1 1 f 3 
+    3 3 f 1 1 9 9 1 1 1 9 9 1 1 f 3 
+    3 3 f f 1 1 1 1 f 1 1 1 f f f 3 
+    3 3 3 f f 1 1 7 7 7 1 f f 3 3 3 
+    3 3 3 3 3 f f 7 3 7 f f 3 3 3 3 
+    3 3 3 3 3 3 f f f f f 3 3 3 3 3 
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+    `, SpriteKind.Player)
